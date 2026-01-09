eaa0e0dedab29fc4e2cfd925d4378928e2504e14
Dmitry Vasiliev <contact.vasiliev.dmitry@gmail.com>
2026-01-09 20:05:49 +0300

    refactor: simplify temp_page fixture and update project metadata

--- tests/test_async/conftest.py
+++ tests/test_async/conftest.py
@@ -1,9 +1,6 @@
-import asyncio
 import typing
 
-import nest_asyncio
 import pytest
-import pytest_asyncio
 from playwright.async_api import Page
 
 from tests.types import TempFileFactoryType
@@ -12,7 +9,7 @@ from tests.utils import make_fs_url
 
 
 @pytest.fixture
-async def temp_page(page_async: Page, temp_page_maker: TempFileFactoryType) -> TempPageFactoryType:
+async def temp_page(page: Page, temp_page_maker: TempFileFactoryType) -> TempPageFactoryType:
     async def wrapper(js_code: typing.Optional[str] = None) -> Page:
         if js_code is None:
             js_code = ""
@@ -20,17 +17,8 @@ async def temp_page(page_async: Page, temp_page_maker: TempFileFactoryType) -> T
         path = temp_page_maker(js_code)
         url = make_fs_url(path)
 
-        await page_async.goto(url)
+        await page.goto(url)
 
-        return page_async
+        return page
 
     return wrapper
-
-
-@pytest_asyncio.fixture(scope="session", autouse=True)
-def event_loop():
-    policy = asyncio.get_event_loop_policy()
-    loop = policy.new_event_loop()
-    nest_asyncio.apply(loop)
-    yield loop
-    loop.close()
